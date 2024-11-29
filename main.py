9f7bc891392ab0f4b102c8587f487d5e1ad70e87
dena <deniskrndzija@googlemail.com>
2024-11-29 15:29:35 +0100

    Neue Funktion zum Addieren von zwei Zahlen hinzugefügt

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hallo, Docker und Git")
+def addiere_zwei_zahlen(a, b):
+    return a + b
+
+zahl1 = 5 
+zahl2 = 7
+ergebnis = addiere_zwei_zahlen(zahl1, zahl2)
+
+
+printf(f"Die Summe von  {zahl1} und {zahl2} ist {ergebnis}.") 
